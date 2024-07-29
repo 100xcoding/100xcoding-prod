b248b1286968776632c3d134a541e8e3fc1edd6b
techysiddhant <sidjain8000@gmail.com>
2024-07-29 19:53:11 +0530

    fix fetcher URL

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -1,7 +1,8 @@
+import Env from "@/lib/env";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${Env.NEXT_PUBLIC_URL}/api`,
 });
 
 const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
