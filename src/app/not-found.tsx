53ff9c765e81bc43f9d9210d10e7931758dc13fa
techysiddhant <sidjain8000@gmail.com>
2024-07-30 01:24:28 +0530

    fix 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,34 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-dark-400  text-white">
-      <h2 className="text-green-500 font-semibold text-4xl capitalize mb-5">
-        There was a problem
-      </h2>
+      <Image
+        src="/error.svg"
+        alt="404 Web illustrations by Storyset"
+        width={"500"}
+        height={"500"}
+      />
+
       <p>We could not find the page you were looking for.</p>
-      <p>
+      <p className="my-3">
         Go back to the{" "}
         <Link
           href="/"
-          className="text-green-500 font-semibold text-lg"
+          className="bg-green-500  text-white  font-semibold text-xl px-4 py-2 rounded-md"
           aria-label="Home Page"
         >
           {" "}
           Home Page
         </Link>{" "}
       </p>
+      <Link
+        href="https://storyset.com/web"
+        className="block mt-4 text-dark-700 text-xs text"
+      >
+        Web illustrations by Storyset
+      </Link>
     </main>
   );
 };
