22745bcc8f1c9ee5ba961c35c3634ec4e90f8937
techysiddhant <sidjain8000@gmail.com>
2024-08-01 19:51:14 +0530

    fix the terms and privacy page as Static Pages

---
+++ src/app/(legal)/terms/page.tsx
@@ -0,0 +1,131 @@
+const TermsPage = () => {
+  return (
+    <main className="bg-dark-300">
+      <section className="text-white py-10 ">
+        <div className="space-y-6 max-w-2xl mx-auto px-4">
+          <h1 className="text-center text-2xl md:text-4xl font-bold tracking-wider">
+            Terms and Conditions
+          </h1>
+          <div className="">
+            <h2 className="text-xl font-semibold">1. Introduction</h2>
+            <p>
+              Welcome to 100xCoding.com. These terms and conditions outline the
+              rules and regulations for the use of our website.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">2. Acceptance of Terms</h2>
+            <p>
+              By accessing or using 100xCoding.com, you agree to comply with and
+              be bound by these terms and conditions. If you do not agree with
+              any part of these terms, you must not use our website.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">3. Changes to Terms</h2>
+            <p>
+              We reserve the right to modify these terms and conditions at any
+              time. Any changes will be effective immediately upon posting on
+              the website. It is your responsibility to review the terms
+              periodically for updates.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">4. Use of Website</h2>
+            <p>
+              You agree to use the website only for lawful purposes and in a way
+              that does not infringe the rights of, restrict, or inhibit anyone
+              else&apos;s use and enjoyment of the website.
+            </p>
+            <p>
+              You must not use the website to distribute any harmful, offensive,
+              or illegal content.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">5. User Accounts</h2>
+            <p>
+              To access certain features of the website, you may need to create
+              an account. You must provide accurate and complete information and
+              keep your account information updated.
+            </p>
+            <p>
+              You are responsible for maintaining the confidentiality of your
+              account and password and for all activities that occur under your
+              account.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">6. Intellectual Property</h2>
+            <p>
+              All content on the website, including text, graphics, logos,
+              images, and software, is the property of 100xCoding.com or its
+              content suppliers and is protected by intellectual property laws.
+            </p>
+            <p>
+              You may not reproduce, distribute, or create derivative works from
+              any content on the website without our express written permission.
+            </p>
+            <p>
+              Certain content on the website may be available under public
+              domain or other licenses that permit reuse. Such content will be
+              clearly marked, and users are allowed to use it in accordance with
+              the terms specified.
+            </p>
+            <p>
+              We provide credit to all public domain sources we use by
+              mentioning their names on our website.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">7. User Content</h2>
+            <p>
+              By submitting content to the website (e.g., comments, project
+              submissions), you grant us a non-exclusive, royalty-free,
+              perpetual, and worldwide license to use, reproduce, modify, and
+              distribute your content in connection with the website.
+            </p>
+            <p>
+              You represent and warrant that you own or have the necessary
+              rights to the content you submit and that your content does not
+              infringe any third-party rights.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">8. Disclaimers</h2>
+            <p>
+              The website and its content are provided on an &quot;as is&quot;
+              and &quot;as available&quot; basis. We make no representations or
+              warranties of any kind, express or implied, regarding the website
+              or its content.
+            </p>
+            <p>
+              We do not warrant that the website will be uninterrupted, secure,
+              or error-free, or that any defects will be corrected.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">
+              9. Limitation of Liability
+            </h2>
+            <p>
+              To the fullest extent permitted by law, we shall not be liable for
+              any direct, indirect, incidental, special, or consequential
+              damages arising out of or in connection with your use of the
+              website.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-semibold">10. Contact Information</h2>
+            <p>
+              If you have any questions about these terms and conditions, please
+              contact us at admin@100xcoding.com.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default TermsPage;
