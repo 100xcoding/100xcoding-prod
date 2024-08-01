d20bd2b1d07fe425c383d6ddc20b1aa50d2b4e0d
techysiddhant <sidjain8000@gmail.com>
2024-08-02 00:59:38 +0530

    add coming soon

--- src/app/(root)/solutions/page.tsx
+++ src/app/(root)/solutions/page.tsx
@@ -14,8 +14,8 @@ const SolutionsPage = async () => {
           solutions.map((solution) => (
             <SolutionCard key={solution.id} {...solution} />
           ))}
-        {!solutions && (
-          <h2 className="text-center font-heading m-10 text-6xl sm:text-7xl lg:text-8xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-black	 ">
+        {solutions && solutions.length <= 0 && (
+          <h2 className="text-center font-heading mx-auto m-10 text-6xl sm:text-7xl lg:text-8xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-black	 ">
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
               {"Coming Soon!"}
             </span>
