af0e7b9644d608a37d1f57b6a72646f25d3cbd8b
techysiddhant <sidjain8000@gmail.com>
2024-08-25 22:45:46 +0530

    add resource item component

--- src/app/(root)/resources/_components/resource-type-item.tsx
+++ src/app/(root)/resources/_components/resource-type-item.tsx
@@ -1,3 +1,17 @@
-export const ResourceTypeItem = () => {
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { cn } from "@/lib/utils";
+import qs from "query-string";
+type ResourceTypeItemProps = {
+  id: string;
+  name: string;
+};
+export const ResourceTypeItem = ({ id, name }: ResourceTypeItemProps) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentCategoryId = searchParams.get("type");
+  // const currentTitle = searchParams.get("tag");
+  const isSelected = currentCategoryId === id;
   return <div>ResourceTypeItem</div>;
 };
